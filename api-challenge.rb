fef24a92a983f109c25756ae4b12ccc8c188f86c
Jeffrey L Olson Jr <jolson@Jeffreys-MacBook-Pro.local>
2017-06-14 13:54:40 -0400

    temporary hotfix for weather

--- api-challenge.rb
+++ api-challenge.rb
@@ -3,39 +3,23 @@ Bundler.require
 require 'pp'
 
 # In these two lines we get the data from spotify and save to the variable called taylor_top_tracks
-response = HTTParty.get('https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU/top-tracks?country=US')
-top_tracks = JSON.parse(response.body)
+# response = HTTParty.get('https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU/top-tracks?country=US')
+response = HTTParty.get('https://www.metaweather.com/api/location/2459115')
+nyc_weather = JSON.parse(response.body, {:symbolize_names => true})
 
-pp top_tracks
+# pp nyc_weather
 
 # Using your ninja array and hash traversal skills:
 
 # Level One:
 
-# 1. Write code to return to print of the number of tracks returned.
+# 1. Print out the name of the city
+puts nyc_weather[:title]
 
+# 2. Print out the "Where on earth ID"
 
-# 2. Write code to print the name of the second track
+# 3. Print out the timezone.
 
+# 4. Print out the weather description of the first weather report.
 
-# 3. Write code to print the duration (in milliseconds) of the first track.
-
-
-# 4. Print the name of the album on the fourth track
-
-
-# 5. Print the names of all of the artists on the third track (there may be more than one line of code here).
-
-# Level Two:
-
-# 6. Write code that prints the names of all of the top songs in the returned hash (use `.each`)
-
-
-# 7. Write code that iterates through each song and organizes the songs by popularity
-
-
-# 8. Write code that iterates through each track and returns the name of all the artists that worked on that track.
-
-# Advanced Project:
-
-# Build a command line program that allows you to input the name or id of an artist, and returns that artist's top tracks.
+# 5. Print out the direction of the wind in the second weather report.
